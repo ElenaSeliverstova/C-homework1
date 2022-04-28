3fe0f8457741c84301012c6806f916133e9b8d9b
Lena <eseliver@icloud.com>
2022-04-28 14:30:37 +0300

    1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+Console.Write("Введите целое число 1 =>");
+int num1 = int.Parse(Console.ReadLine ());
+Console.Write("Введите целое число 2 =>");
+int num2 = int.Parse(Console.ReadLine ());
+if(num1>num2) Console.WriteLine("Число 1 больше числа 2");
+if(num2>num1) Console.WriteLine("Число 2 больше числа 1");
+
